6a9f79653c00bb9b37c8a52dfeae274d6c8be36d
Filip Razek <filip.razek@protonmail.com>
2023-09-01 16:30:47 +0200

    Update styles for real

--- src/components/ChannelSelect/index.jsx
+++ src/components/ChannelSelect/index.jsx
@@ -1,26 +1,29 @@
 import { StyleSheet, Text, View } from "react-native";
+import SelectDropdown from "react-native-select-dropdown";
 
-export const ChannelSelect = ({ options }) => {
-  return (
+export const ChannelSelect = ({ options, label }) => {
+  return options.length > 0 ? (
     <View style={styles.container}>
-      <Text>X:</Text>
-      {/* {options.length > 0 ? (
-        <RNPickerSelect
-          onValueChange={(value) => console.log(value)}
-          items={options.map((option) => ({ label: option, value: option }))}
-        />
-      ) : null} */}
+      <Text style={styles.label}>{label}:</Text>
+      <SelectDropdown
+        onValueChange={(value) => console.log(value)}
+        data={options}
+      />
     </View>
-  );
+  ) : null;
 };
 
 const styles = StyleSheet.create({
   container: {
-    flex: 5,
+    flex: 1,
     flexDirection: "row",
     alignItems: "center",
     justifyContent: "center",
-    backgroundColor: "pink",
+    marginVertical: 20,
+  },
+  label: {
+    marginHorizontal: 20,
+    fontSize: 20,
+    textTransform: "uppercase",
   },
 });
-
--- src/screens/Home/index.jsx
+++ src/screens/Home/index.jsx
@@ -1,14 +1,9 @@
 import React from "react";
-import {
-  StyleSheet,
-  StatusBar,
-  SafeAreaView,
-  View,
-  Button,
-} from "react-native";
+import { StyleSheet, SafeAreaView, View, Button } from "react-native";
 import { readString } from "react-native-csv";
 import { FilePicker } from "../../components/FilePicker";
 import { ChannelSelect } from "../../components/ChannelSelect";
+import Svg, { Path } from "react-native-svg";
 
 function extractData(csvString) {
   const convertedData = readString(csvString).data;
@@ -26,59 +21,48 @@ export const Home = () => {
     setTableData(extractData(newFileData));
   }
 
+  function changeGraph() {
+    console.log("change graph");
+  }
+
+  function changeSettings() {
+    console.log("change settings");
+  }
+
   return (
-    <SafeAreaView>
-      <StatusBar barStyle={"dark-content"} />
-      <View style={styles.container}>
-        <View style={styles.header}></View>
-        <FilePicker updateData={updateDataFromFile} />
-        <ChannelSelect options={tableData[0]} />
-        <ChannelSelect options={tableData[0]} />
-        <View class="graph-settings" style={{ flex: 1 }}>
-          <Button class="icon-button" title="">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              enable-background="new 0 0 24 24"
-              height="24px"
-              viewBox="0 0 24 24"
-              width="24px"
-              fill="#000000"
-            >
-              <g>
-                <rect fill="none" height="24" width="24" />
-              </g>
-              <g>
-                <g>
-                  <rect height="11" width="4" x="4" y="9" />
-                  <rect height="7" width="4" x="16" y="13" />
-                  <rect height="16" width="4" x="10" y="4" />
-                </g>
-              </g>
-            </svg>
-          </Button>
-          <Button class="icon-button" title="">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              height="24px"
-              viewBox="0 0 24 24"
-              width="24px"
-              fill="#000000"
-            >
-              <path d="M0 0h24v24H0V0z" fill="none" />
-              <path d="M3 17v2h6v-2H3zM3 5v2h10V5H3zm10 16v-2h8v-2h-8v-2h-2v6h2zM7 9v2H3v2h4v2h2V9H7zm14 4v-2H11v2h10zm-6-4h2V7h4V5h-4V3h-2v6z" />
-            </svg>
-          </Button>
+    <SafeAreaView style={styles.container}>
+      <FilePicker updateData={updateDataFromFile} />
+      <ChannelSelect label="x" options={tableData[0]} />
+      <ChannelSelect label="y" options={tableData[0]} />
+      {tableData[0].length ? (
+        <View style={styles.graphOptions}>
+          <Svg
+            onPress={changeGraph}
+            width={50}
+            height={50}
+            style={styles.iconButton}
+            viewBox={"0 0 24 24"}
+          >
+            <Path fill="none" d="M0 0h24v24H0z" />
+            <Path d="M4 9h4v11H4zM16 13h4v7h-4zM10 4h4v16h-4z" />
+          </Svg>
+          <Svg
+            onPress={changeSettings}
+            width={50}
+            height={50}
+            style={styles.iconButton}
+            viewBox={"0 0 24 24"}
+          >
+            <Path fill="none" d="M0 0h24v24H0V0z" />
+            <Path d="M3 17v2h6v-2H3zM3 5v2h10V5H3zm10 16v-2h8v-2h-8v-2h-2v6h2zM7 9v2H3v2h4v2h2V9H7zm14 4v-2H11v2h10zm-6-4h2V7h4V5h-4V3h-2v6z" />
+          </Svg>
         </View>
-        <View class="generate-button-div" style={{ flex: 1 }}>
+      ) : null}
+      {tableData[0].length ? (
+        <View style={styles.generateButton}>
           <Button title="Generate"></Button>
         </View>
-      </View>
-      <View
-        style={[styles.diagonalUnderlay, styles.diagonalUnderlayTop]}
-      ></View>
-      <View
-        style={[styles.diagonalUnderlay, styles.diagonalUnderlayBottom]}
-      ></View>
+      ) : null}
     </SafeAreaView>
   );
 };
@@ -86,25 +70,31 @@ export const Home = () => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    padding: "10%",
   },
-  header: {
-    backgroundColor: "purple",
-    flex: 2,
+  graphOptions: {
+    flex: 1,
+    flexDirection: "row",
+    justifyContent: "center",
+    alignItems: "center",
+    width: "100%",
   },
-  diagonalUnderlay: {
-    position: "absolute",
-    backgroundColor: "pink",
-    opacity: 0.4,
-    zIndex: -1,
-    width: "40%",
-    height: "25%",
+  generateButton: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    width: "100%",
   },
-  diagonalUnderlayTop: {
-    clipPath: "polygon(100% 0, 0 0, 0 100%)",
-  },
-  diagonalUnderlayBottom: {
-    right: 0,
-    bottom: 0,
-    clipPath: "polygon(100% 0, 100% 100%, 0 100%)",
+  iconButton: {
+    flex: 1,
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "lightgray",
+    borderRadius: 50,
+    padding: 20,
+    margin: 10,
   },
 });
