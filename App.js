dd615ad697278005f0bd9d5a5929d8969186980e
Filip Razek <filip@razek.net>
2023-09-01 11:50:24 +0200

    Add basic FS implementation

--- App.js
+++ App.js
@@ -1,35 +1,42 @@
 import React from "react";
-import { StyleSheet, Text, Button, StatusBar, SafeAreaView } from "react-native";
+import {
+  StyleSheet,
+  Text,
+  Button,
+  StatusBar,
+  SafeAreaView,
+} from "react-native";
 import DocumentPicker from "react-native-document-picker";
+import { readFile } from "react-native-fs";
+import { readString } from "react-native-csv";
 
 export default function App() {
-  const [fileResponse, setFileResponse] = React.useState([]);
+  const [csvFile, setCsvFile] = React.useState([]);
 
-  const handleDocumentSelection = React.useCallback(async () => {
+  const handleDocumentSelection = async function () {
     try {
       const response = await DocumentPicker.pick({
         presentationStyle: "fullScreen",
-        // type: [DocumentPicker.types.csv],
       });
-      setFileResponse(response);
+      const chosenFile = response[0].uri;
+      const fileData = await readFile(chosenFile, "utf8");
+      const convertedData = readString(fileData);
+      console.log(convertedData);
+
+      setCsvFile(chosenFile);
     } catch (err) {
-      console.warn(err);
+      if (DocumentPicker.isCancel(err)) {
+        console.log("User cancelled the picker");
+      } else console.warn(err);
     }
-  }, []);
+  };
 
   return (
     <SafeAreaView style={styles.container}>
       <StatusBar barStyle={"dark-content"} />
-      {fileResponse.map((file, index) => (
-        <Text
-          key={index.toString()}
-          style={styles.uri}
-          numberOfLines={1}
-          ellipsizeMode={"middle"}
-        >
-          {file?.uri}
-        </Text>
-      ))}
+      <Text style={styles.uri} numberOfLines={1} ellipsizeMode={"middle"}>
+        {csvFile}
+      </Text>
       <Button title="Select 📑" onPress={handleDocumentSelection} />
     </SafeAreaView>
   );
